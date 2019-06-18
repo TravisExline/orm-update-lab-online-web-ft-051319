fb0ca0267d030677a11f993afc82523c41288429
Travis Exline <t.exline22@gmail.com>
2019-06-18 14:12:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,11 +1,11 @@
 require_relative "../config/environment.rb"
 
 class Student
-  attr_accessor :name, :grade 
-  attr_reader :id 
+  attr_accessor :name, :grade
+  attr_reader :id
 
   def initialize(name, grade, id = nil)
-    @name = name 
+    @name = name
     @grade = grade
   end
 
