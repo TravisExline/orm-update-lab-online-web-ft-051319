0d21e5f40be7d4cb06fd28630acca775d9c33938
Travis Exline <t.exline22@gmail.com>
2019-06-18 14:14:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,6 +9,15 @@ class Student
     @grade = grade
   end
 
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+  end
+
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
 
