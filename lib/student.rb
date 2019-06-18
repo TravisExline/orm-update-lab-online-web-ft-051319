a0e998d34e09b7f77804f6e2ebad74697e2e5f08
Travis Exline <t.exline22@gmail.com>
2019-06-18 14:27:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,10 +46,10 @@ class Student
    end
 
    def self.new_from_db(row)
-  #   new_student = self.new
-  #   new_student.id = row[0]
-  #   new_student.name = row[1]
-  #   new_student.grade = row[2]
+    new_student = self.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
    end
 
   # Remember, you can access your database connection anywhere in this class
