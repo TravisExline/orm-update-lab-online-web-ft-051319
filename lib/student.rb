efd34342ee702027a732be88c3489463383827e3
Travis Exline <t.exline22@gmail.com>
2019-06-18 14:33:26 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,6 +57,15 @@ class Student
    end
 
    def self.find_by_name(name)
+     sql = <<-SQL
+       SELECT * FROM students
+       WHERE name = ?
+       LIMIT 1
+       SQL
+
+     DB[:conn].execute(sql, name).map do |row|
+       self.new_from_db(row)
+     end
    end
 
 
